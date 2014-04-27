64ac7259c3f620aa811eac97ebbb6e6c2a7b2a52
Kingclove <klouse@outlook.com>
2014-04-27 18:52:45 -0500

    Import Channel API code and partially implemented it.

--- main.py
+++ main.py
@@ -7,28 +7,82 @@ import os
 import sys
 
 # sys.path includes 'server/lib' due to appengine_config.py
-from flask import Flask
+from flask import Flask , request
 from flask import render_template
+import urllib2
+import json
+import random
+import uuid
+from google.appengine.api import users
+from google.appengine.api import channel
+from utilities import funnames
 app = Flask(__name__.split('.')[0])
 
 
 @app.route('/')
-def menu(name=None):
+def menu():
   """ Return hello template at application root URL."""
-  return render_template('menu.html', name=name)
+  return render_template('menu.html')
 
 @app.route('/cards')
-def oneplayer(name=None):
+def oneplayer():
   """ Return hello template at application root URL."""
-  return render_template('cards.html', name=name)
+  return render_template('cards.html')
 
 @app.route('/multi')
-def multiplayer(name=None):
+def multiplayer():
   """ Return hello template at application root URL."""
-  return render_template('multi.html', name=name)
+  return render_template('multi.html')
 
 @app.route('/playernames')
-def names(name=None):
+def names():
   """ Return hello template at application root URL."""
-  return render_template('playernames.html', name=name)
+  return render_template('playernames.html')
 
+@app.route('/online_play')
+def online_setup():
+  """ Returns page for online setup"""
+  roomid = generate_id()
+  template_values = {"roomid":roomid}
+  return render_template("online_setup.html",values=template_values)
+
+@app.route('/<roomid>/<name>')
+def host_setup(roomid,name):
+    """Return a chat page"""
+    token = channel.create_channel(name + roomid) 
+    template_values = {
+                       "roomid":roomid,
+                        "token": token,
+                        "yourname": name
+                       }
+    return render_template("host.html",values=template_values)
+
+
+
+@app.route('/<roomid>/<name>/guest')
+def guest_setup(roomid,name):
+    """Return a chat page"""
+    template_values = {
+                       "roomid":roomid,
+                        "opponent": name
+                       }
+    return render_template("guest_setup.html",values=template_values)
+
+
+
+@app.route('/<roomid>/guest/<name>')
+def host_setup(roomid,name):
+    """Return a chat page"""
+    token = channel.create_channel(name + roomid) 
+    template_values = {
+                       "roomid":roomid,
+                        "token": token,
+                        "yourname": name
+                       }
+    return render_template("guest.html",values=template_values)
+
+
+
+def generate_id():
+    """Return a game id"""
+    return "%s-%s" % (str(uuid.uuid4())[:4],random.choice(funnames).lower())
